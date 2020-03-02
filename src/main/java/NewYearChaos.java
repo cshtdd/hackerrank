554a0c3204fa19e92ac51d6bd71233d11da2cc40
Camilo Sanchez <camilin87@gmail.com>
2020-03-02 11:35:10 -0500

    Optimize too chaotic calculation

--- src/main/java/NewYearChaos.java
+++ src/main/java/NewYearChaos.java
@@ -4,10 +4,20 @@ import java.util.Optional;
 public class NewYearChaos {
     public static void main(String[] args) {
         minimumBribes(new int[]{2, 1, 5, 3, 4});
+        minimumBribes(new int[]{2, 5, 1, 3, 4});
     }
 
     static void minimumBribes(int[] q) {
         HashMap<Integer, Integer> rotations = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < q.length - 1; i++) {
+            int currentValue = q[i];
+            int originalIndex = currentValue - 1;
+            if (originalIndex > i + 2){
+                System.out.println("Too chaotic");
+                return;
+            }
+        }
 
         for (int i = 0; i < q.length - 1; i++) {
             int currentValue = q[i];
@@ -21,10 +31,10 @@ public class NewYearChaos {
                 } else {
                     int rotationsCount = rotations.get(currentValue) + 1;
 
-                    if (rotationsCount > 2){
-                        System.out.println("Too chaotic");
-                        return;
-                    }
+//                    if (rotationsCount > 2){
+//                        System.out.println("Too chaotic");
+//                        return;
+//                    }
 
                     rotations.put(currentValue, rotationsCount);
                 }
