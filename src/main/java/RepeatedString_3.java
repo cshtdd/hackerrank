46bb8937f9ae7c53a665b7ffd80125a0cd09ef76
Camilo Sanchez <camilin87@gmail.com>
2020-03-02 07:45:36 -0500

    Minor optimization that did not yield the expected results

--- src/main/java/RepeatedString_3.java
+++ src/main/java/RepeatedString_3.java
@@ -1,14 +1,19 @@
+import java.util.Arrays;
+
 public class RepeatedString_3 {
     static long repeatedString(String s, long n) {
-        if (s.chars().allMatch(c -> c == 'a')){
+        int[] chars = s.chars().toArray();
+        int length = chars.length;
+
+        if (Arrays.stream(chars).allMatch(c -> c == 'a')){
             return n;
         }
 
         int result = 0;
 
         for (int i = 0; i < n; i++) {
-            int index = i % s.length();
-            if ('a' == s.charAt(index)){
+            int index = i % length;
+            if ('a' == chars[index]){
                 result++;
             }
         }
