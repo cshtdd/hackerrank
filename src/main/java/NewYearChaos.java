512d307709bd9f5346fb466a8086d39566fd2d2d
Camilo Sanchez <camilin87@gmail.com>
2020-03-02 13:29:34 -0500

    O(n) solution for the chaotic queue

--- src/main/java/NewYearChaos.java
+++ src/main/java/NewYearChaos.java
@@ -5,42 +5,46 @@ public class NewYearChaos {
     public static void main(String[] args) {
         minimumBribes(new int[]{2, 1, 5, 3, 4});
         minimumBribes(new int[]{2, 5, 1, 3, 4});
+        minimumBribes(new int[]{5, 1, 2, 3, 7, 8, 6, 4});
+        minimumBribes(new int[]{1, 2, 5, 3, 7, 8, 6, 4});
+        minimumBribes(new int[]{1, 2, 5, 3, 4, 7, 8, 6});
     }
 
     static void minimumBribes(int[] q) {
-        HashMap<Integer, Integer> rotations = new HashMap<Integer, Integer>();
+        int result = 0;
 
-        for (int i = 0; i < q.length - 1; i++) {
+        for (int i = 0; i < q.length; i++) {
             int currentValue = q[i];
-
             int originalIndex = currentValue - 1;
-            if (originalIndex > i + 2){
+
+            if (originalIndex > i + 2) {
                 System.out.println("Too chaotic");
                 return;
             }
+        }
 
+        for (int i = 1; i < q.length - 1; i++) {
+            int prevValue = q[i - 1];
+            int currentValue = q[i];
             int nextValue = q[i + 1];
 
-            if (currentValue > nextValue) {
-                swap(q, i, i + 1);
-                increment(rotations, currentValue);
-                i = -1;
+            if (prevValue < currentValue && currentValue < nextValue) {
+                continue;
             }
+
+            result++;
+
+            if (prevValue > currentValue) {
+                swap(q, i, i - 1);
+                i = Math.max(0, i - 2);
+                continue;
+            }
+
+            swap(q, i, i + 1);
+            i = Math.max(0, i - 1);
         }
 
-        int result = rotations.values().stream().reduce((x, y) -> x + y).get();
         System.out.println(result);
-    }
-
-    private static int increment(HashMap<Integer, Integer> map, int key){
-        if (!map.containsKey(key)) {
-            map.put(key, 1);
-            return 1;
-        }
-
-        int result = map.get(key) + 1;
-        map.put(key, result);
-        return result;
     }
 
     private static void swap(int[] arr, int index1, int index2) {
