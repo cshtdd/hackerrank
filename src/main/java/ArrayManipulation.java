98418d4d89db14be73e01982a5f31bb18d53aefc
Camilo Sanchez <camilin87@gmail.com>
2020-03-03 11:41:57 -0500

    Implement the ArrayManipulation in O(n)

--- src/main/java/ArrayManipulation.java
+++ src/main/java/ArrayManipulation.java
@@ -1,30 +1,26 @@
 public class ArrayManipulation {
     static long arrayManipulation(int n, int[][] queries) {
         long[] arr = new long[n];
-        long result = -1;
 
         for (int i = 0; i < queries.length; i++) {
             int startIndex = queries[i][0] - 1;
-            int endIndex = queries[i][1] - 1;
+            int endIndex = queries[i][1];
             int increment = queries[i][2];
 
-            long largestValue = incrementArray(arr, startIndex, endIndex, increment);
-            if (largestValue > result){
-                result = largestValue;
+            arr[startIndex] += increment;
+            if (endIndex < n){
+                arr[endIndex] -= increment;
             }
         }
 
-        return result;
-    }
+        long result = arr[0];
+        long buffer = arr[0];
 
-    private static long incrementArray(long[] arr, int startIndex, int endIndex, int increment) {
-        long result = -1;
+        for (int i = 1; i < n; i++) {
+            buffer += arr[i];
 
-        for (int i = startIndex; i <= endIndex; i++) {
-            arr[i] += increment;
-
-            if (arr[i] > result) {
-                result = arr[i];
+            if (buffer > result){
+                result = buffer;
             }
         }
 
